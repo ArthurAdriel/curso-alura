eb8cb7a25f2fec29df813f1b57605695a1a96156
Arthur Adriel <arthurtorquato25@gmail.com>
2026-07-27 10:18:14 -0300

    .

---
+++ ex025.js
@@ -0,0 +1,6 @@
+const grupo1 = ['Arthur', 'Adriel']
+const grupo2 = ['Torquato', 'Almeida']
+
+const juncao = grupo1.concat(grupo2)
+
+console.log(juncao)
---
+++ ex028.js
@@ -0,0 +1,4 @@
+const menuPrincipal = ['Strogonoff', 'Risoto', 'Carbonara', 'Lasanha']
+const menuDeSobremesas = ['Pudim', 'Sorvete', 'Mousse de Maracujá']
+const menuCompleto = menuPrincipal.concat(menuDeSobremesas)
+console.log(menuCompleto)
