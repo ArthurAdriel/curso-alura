969f4dd10e01c0f866d815beaa185170eaa83a99
Arthur Adriel <arthurtorquato25@gmail.com>
2026-09-22 23:41:32 -0300

    .
    ex 048 a 050

---
+++ ex048.js
@@ -0,0 +1,11 @@
+const jogador = {
+    nome: "Fred",
+    idade: 33,
+    time: "Atlético-MG"
+};
+
+console.log(jogador)
+
+jogador.idade = 34
+
+console.log(jogador)
---
+++ ex050.js
@@ -0,0 +1,8 @@
+const livro = {
+    nome: "A casa das palavras",
+    autor: "Marina Colasanti",
+    anoPublicacao: 2002,
+    genero: "Crônicas"
+};
+
+livro.anoAtual = 
